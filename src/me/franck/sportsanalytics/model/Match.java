f7d8940293e8be7950f16f97d15fa11951264ded
Franck Tedjou <262764190+franck-ted-dev@users.noreply.github.com>
2026-05-01 01:53:26 +0200

    first implementation of Match

--- src/me/franck/sportsanalytics/model/Match.java
+++ src/me/franck/sportsanalytics/model/Match.java
@@ -1,4 +1,31 @@
 package me.franck.sportsanalytics.model;
 
 public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+
+    public Match(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
 }
